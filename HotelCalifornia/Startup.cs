e0cab3194eeb6028ed52efe1e9b95e9085d79af3
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-21 19:44:57 +0200

    feat: add services to startup

--- HotelCalifornia/Startup.cs
+++ HotelCalifornia/Startup.cs
@@ -1,9 +1,14 @@
-using HotelCalifornia.Backend.Shared.Settings;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.ResponseCompression;
+using HotelCalifornia.Configuration;
+using HotelCalifornia.Backend.Shared.Settings;
+using HotelCalifornia.Backend.Shared.Environment;
 
 namespace HotelCalifornia
 {
@@ -20,6 +25,37 @@ namespace HotelCalifornia
         
         public void ConfigureServices(IServiceCollection AServices)
         {
+            AServices.AddMvc(AOption => AOption.CacheProfiles
+                .Add("Standard", new CacheProfile
+                { 
+                    Duration = 10, 
+                    Location = ResponseCacheLocation.Any, 
+                    NoStore = false 
+                }));
+
+            AServices.AddMvc();
+            AServices.AddControllers();          
+            AServices.AddSpaStaticFiles(AOptions => AOptions.RootPath = "ClientApp/build");
+            AServices.AddResponseCompression(AOptions => AOptions.Providers.Add<GzipCompressionProvider>());
+            
+            // For E2E testing only when application is bootstrapped in memory
+            if (EnvironmentVariables.IsStaging())
+                Dependencies.RegisterForTests(AServices, FConfiguration);
+            
+            // Local development
+            if (FEnvironment.IsDevelopment())
+            {
+                Dependencies.RegisterForTests(AServices, FConfiguration);
+
+                AServices.AddSwaggerGen(AOption =>
+                    AOption.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelCaliforniaApi", Version = "v1" }));
+            }
+
+            // Production and Staging (deployment slots only)
+            if (!FEnvironment.IsProduction() && !FEnvironment.IsStaging()) 
+                return;
+
+            Dependencies.Register(AServices, FConfiguration);
         }
 
         public void Configure(IApplicationBuilder AApplication, AppUrls AAppUrls)
