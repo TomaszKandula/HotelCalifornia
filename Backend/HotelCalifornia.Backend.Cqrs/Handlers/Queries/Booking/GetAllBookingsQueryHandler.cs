b1953f86175f970ffcf21f2417ca84b5f70b02ca
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-21 23:29:15 +0200

    feat: add cqrs handler implementation

--- Backend/HotelCalifornia.Backend.Cqrs/Handlers/Queries/Booking/GetAllBookingsQueryHandler.cs
+++ Backend/HotelCalifornia.Backend.Cqrs/Handlers/Queries/Booking/GetAllBookingsQueryHandler.cs
@@ -1,17 +1,38 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using HotelCalifornia.Backend.Database;
 
 namespace HotelCalifornia.Backend.Cqrs.Handlers.Queries.Booking
 {
     public class GetAllArticlesQueryHandler : TemplateHandler<GetAllBookingsQuery, IEnumerable<GetAllBookingsQueryResult>>
     {
-        public GetAllArticlesQueryHandler() { }
+        private readonly DatabaseContext FDatabaseContext;
+        
+        public GetAllArticlesQueryHandler(DatabaseContext ADatabaseContext) 
+            => FDatabaseContext = ADatabaseContext;
 
         public override async Task<IEnumerable<GetAllBookingsQueryResult>> Handle(GetAllBookingsQuery ARequest, CancellationToken ACancellationToken)
         {
-
-            return new List<GetAllBookingsQueryResult>();
+            var LBookings = await FDatabaseContext.Bookings
+                .AsNoTracking()
+                .Include(ABookings => ABookings.Room)
+                .Select(ABookings => new GetAllBookingsQueryResult
+                {
+                    Id = ABookings.Id,
+                    RoomNumber = ABookings.Room.Bedrooms,
+                    Bedrooms = ABookings.Room.RoomNumber,
+                    GuestFullName = ABookings.GuestFullName,
+                    GuestPhoneNumber = ABookings.GuestPhoneNumber,
+                    DateFrom = ABookings.DateFrom,
+                    DateTo = ABookings.DateTo
+                })
+                .OrderByDescending(ABookings => ABookings.GuestFullName)
+                .ToListAsync(ACancellationToken);
+            
+            return LBookings;
         }
     }
 }
--- Backend/HotelCalifornia.Backend.Cqrs/Handlers/Queries/Booking/GetAllBookingsQueryResult.cs
+++ Backend/HotelCalifornia.Backend.Cqrs/Handlers/Queries/Booking/GetAllBookingsQueryResult.cs
@@ -4,6 +4,18 @@ namespace HotelCalifornia.Backend.Cqrs.Handlers.Queries.Booking
 {
     public class GetAllBookingsQueryResult
     {
+        public Guid Id { get; set; }
         
+        public int RoomNumber { get; set; }
+        
+        public int Bedrooms { get; set; }
+        
+        public string GuestFullName { get; set; }
+        
+        public string GuestPhoneNumber { get; set; }
+        
+        public DateTime DateFrom { get; set; }
+
+        public DateTime DateTo { get; set; }
     }
 }
