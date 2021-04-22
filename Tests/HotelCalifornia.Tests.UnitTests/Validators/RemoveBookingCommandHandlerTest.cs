7abd3dca7748b644bad59c4e1d939ff5381469e9
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-22 18:58:46 +0200

    test: add unit tests for remove booking validator

---
+++ Tests/HotelCalifornia.Tests.UnitTests/Validators/RemoveBookingCommandHandlerTest.cs
@@ -0,0 +1,46 @@
+using Xunit;
+using FluentAssertions;
+using System;
+using HotelCalifornia.Backend.Shared.Resources;
+using HotelCalifornia.Backend.Cqrs.Handlers.Commands.Booking;
+
+namespace HotelCalifornia.Tests.UnitTests.Validators
+{
+    public class RemoveBookingCommandHandlerTest
+    {
+        [Fact]
+        public void RemoveArticle_WhenIdIsCorrect_ShouldFinishSuccessfully() 
+        {
+            // Arrange
+            var LRemoveBookingCommand = new RemoveBookingCommand
+            {
+                Id = Guid.NewGuid()
+            };
+
+            // Act
+            var LValidator = new RemoveBookingCommandValidator();
+            var LResult = LValidator.Validate(LRemoveBookingCommand);
+
+            // Assert
+            LResult.Errors.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void RemoveArticle_WhenIdIsIncorrect_ShouldThrowError()
+        {
+            // Arrange
+            var LRemoveArticleCommand = new RemoveBookingCommand
+            {
+                Id = Guid.Empty
+            };
+        
+            // Act
+            var LValidator = new RemoveBookingCommandValidator();
+            var LResult = LValidator.Validate(LRemoveArticleCommand);
+        
+            // Assert
+            LResult.Errors.Count.Should().Be(1);
+            LResult.Errors[0].ErrorCode.Should().Be(nameof(ValidationCodes.REQUIRED));
+        }        
+    }
+}
