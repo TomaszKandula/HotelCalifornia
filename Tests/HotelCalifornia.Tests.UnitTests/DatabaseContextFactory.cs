a45e31d2f955d19cc8203994a0900e7789266641
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-22 18:47:56 +0200

    feat: add database factory for unit testing

---
+++ Tests/HotelCalifornia.Tests.UnitTests/DatabaseContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using HotelCalifornia.Backend.Database;
+
+namespace HotelCalifornia.Tests.UnitTests
+{
+    internal class DatabaseContextFactory
+    {
+        private readonly DbContextOptionsBuilder<DatabaseContext> FDatabaseOptions = new DbContextOptionsBuilder<DatabaseContext>()
+            .UseQueryTrackingBehavior(QueryTrackingBehavior.TrackAll)
+            .EnableSensitiveDataLogging()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+        public DatabaseContext CreateDatabaseContext()
+            =>  new DatabaseContext(FDatabaseOptions.Options);
+    }
+}
---
+++ Tests/HotelCalifornia.Tests.UnitTests/TestBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using HotelCalifornia.Backend.Database;
+
+namespace HotelCalifornia.Tests.UnitTests
+{
+    public class TestBase
+    {
+        private readonly DatabaseContextFactory FDatabaseContextFactory;
+
+        protected TestBase() 
+        {
+            var LServices = new ServiceCollection();
+
+            LServices.AddSingleton<DatabaseContextFactory>();
+            LServices.AddScoped(AContext =>
+            {
+                var LFactory = AContext.GetService<DatabaseContextFactory>();
+                return LFactory?.CreateDatabaseContext();
+            });
+
+            var LServiceScope = LServices.BuildServiceProvider(true).CreateScope();
+            var LServiceProvider = LServiceScope.ServiceProvider;
+            FDatabaseContextFactory = LServiceProvider.GetService<DatabaseContextFactory>();
+        }
+
+        protected DatabaseContext GetTestDatabaseContext()
+            =>  FDatabaseContextFactory.CreateDatabaseContext();
+    }
+}
