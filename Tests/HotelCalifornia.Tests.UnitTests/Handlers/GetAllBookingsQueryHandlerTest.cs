5f490d81de27de034bc790e97a230f530977cf11
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-22 21:15:31 +0200

    test: add new unit tests for query handler

---
+++ Tests/HotelCalifornia.Tests.UnitTests/Handlers/GetAllBookingsQueryHandlerTest.cs
@@ -0,0 +1,69 @@
+using Xunit;
+using FluentAssertions;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using HotelCalifornia.Tests.TestData;
+using HotelCalifornia.Backend.Domain.Entities;
+using HotelCalifornia.Backend.Core.Services.DateTimeService;
+using HotelCalifornia.Backend.Cqrs.Handlers.Queries.Booking;
+
+namespace HotelCalifornia.Tests.UnitTests.Handlers
+{
+    public class GetAllBookingsQueryHandlerTest : TestBase
+    {
+        private readonly DateTimeService FDateTimeService;
+
+        public GetAllBookingsQueryHandlerTest() => FDateTimeService = new DateTimeService();
+
+        [Fact]
+        public async Task GetAllBookings_ShouldReturnsCollection()
+        {
+            // Arrange
+            var LDatabaseContext = GetTestDatabaseContext();
+            var LGetAllBookingsQueryHandler = new GetAllBookingsQueryHandler(LDatabaseContext);
+           
+            var LRoom = new Rooms
+            {
+                RoomNumber = 101,
+                Bedrooms = 3
+            };
+
+            await LDatabaseContext.Rooms.AddAsync(LRoom);
+            await LDatabaseContext.SaveChangesAsync();
+            
+            var LBookings = new List<Bookings>
+            {
+                new Bookings
+                {
+                    RoomId = LRoom.Id,
+                    GuestFullName = DataProvider.GetRandomString(),
+                    GuestPhoneNumber = DataProvider.GetRandomString(9),
+                    DateFrom = FDateTimeService.Now.AddDays(5),
+                    DateTo = FDateTimeService.Now.AddDays(15)
+                },
+                new Bookings
+                {
+                    RoomId = LRoom.Id,
+                    GuestFullName = DataProvider.GetRandomString(),
+                    GuestPhoneNumber = DataProvider.GetRandomString(9),
+                    DateFrom = FDateTimeService.Now.AddDays(1),
+                    DateTo = FDateTimeService.Now.AddDays(3)
+                }
+            };
+
+            await LDatabaseContext.Bookings.AddRangeAsync(LBookings);
+            await LDatabaseContext.SaveChangesAsync();
+            
+            // Act
+            var LResults = (await LGetAllBookingsQueryHandler
+                .Handle(new GetAllBookingsQuery(), CancellationToken.None))
+                .ToList();
+            
+            // Assert
+            LResults.Should().NotBeNull();
+            LResults.Should().HaveCount(2);
+        }
+    }
+}
