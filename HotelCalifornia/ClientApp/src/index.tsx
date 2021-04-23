a59e8a0e544433bccefc1fcec9c1a6e6cc0bc1c5
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-24 00:15:15 +0200

    refactor: add redux store and history

--- HotelCalifornia/ClientApp/src/index.tsx
+++ HotelCalifornia/ClientApp/src/index.tsx
@@ -1,17 +1,24 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import configureStore from "./Redux/configureStore";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+
+// Create browser history to use in the Redux store
+const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href") as string;
+const history = createBrowserHistory({ basename: baseUrl });
+
+// Get the application-wide store instance, 
+// prepopulating with state from the server where available.
+const store = configureStore(history);
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
+    </Provider>,
     document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
