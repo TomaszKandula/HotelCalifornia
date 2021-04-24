aeb1f285229c98c364256f9646894d88f66d8a9b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-24 15:13:58 +0200

    feat: add theme with custom colours

---
+++ HotelCalifornia/ClientApp/src/Theme/customColours.ts
@@ -0,0 +1,27 @@
+export const CustomColours =
+{
+    background:
+    {
+        white: "#FFFFFF",
+        ghostWhite: "#F8F8FF",
+        lightGray1: "#FAFAFA",
+        lightGray2: "#EFEFEF",
+        gray1: "#9E9E9E",
+        gray2: "#757575",
+        black: "#000000"
+    },
+    typography:
+    {
+        white: "#FFFFFF",
+        ghostWhite: "#F8F8FF",
+        gray1: "#9E9E9E",
+        gray2: "#757575",
+        darkGray1: "#616161",
+        darkGray2: "#212121",
+        black: "#000000"
+    },
+    application:
+    {
+        navigationBlue: "#1976D2"
+    }
+}
