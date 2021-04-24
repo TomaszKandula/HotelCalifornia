33a8a0b3f5c8b69b0618677339da01e4665c85c4
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-24 16:30:06 +0200

    feat: add text wrappers

---
+++ HotelCalifornia/ClientApp/src/Shared/textWrappers.ts
@@ -0,0 +1,39 @@
+import { ConvertPropsToFields, HtmlRenderLines } from "./helpers";
+import { 
+    BOOKING_ERROR, 
+    BOOKING_SUCCESS,
+    BOOKING_WARNING,
+    UNEXPECTED_STATUS
+} from "../Shared/constants";
+
+// BOOKINGS
+
+const BookingSuccess = (): string =>
+{
+    return BOOKING_SUCCESS;
+}
+
+const BookingWarning = (object: any): string =>
+{
+    return BOOKING_WARNING.replace("{LIST}", HtmlRenderLines(ConvertPropsToFields(object), "li"));
+}
+
+const BookingError = (error: string): string =>
+{
+    return BOOKING_ERROR.replace("{ERROR}", error);
+}
+
+// OTHER
+
+const UnexpectedStatusCode = (statusCode: number): string =>
+{
+    return UNEXPECTED_STATUS.replace("{STATUS_CODE}", statusCode.toString());
+}
+
+export 
+{
+    BookingSuccess,
+    BookingWarning,
+    BookingError,
+    UnexpectedStatusCode
+}
