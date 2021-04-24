10a647ce8e0608a9cf8a5d3a5dc151491ca30bec
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-24 16:28:23 +0200

    feat: add new application constants

--- HotelCalifornia/ClientApp/src/Shared/constants.ts
+++ HotelCalifornia/ClientApp/src/Shared/constants.ts
@@ -6,4 +6,13 @@ export const API_QUERY_GET_BOOKINGS = `${APP_BACKEND}/api/v${API_VER}/booking/Ge
 export const API_COMMAND_ADD_BOOKING = `${APP_BACKEND}/api/v${API_VER}/booking/AddBooking`;
 export const API_COMMAND_REMOVE_BOOKING = `${APP_BACKEND}/api/v${API_VER}/booking/RemoveBooking`;
 
+export const BOOKING_SUCCESS = `<p>Congratulations!</p><p>Your room has been booked!</p>`;
+export const BOOKING_WARNING = `<span>We cannot book your room, following warning(s) received:</span><ul>{LIST}</ul><span>Please make sure the fields are valid.</span>`;
+export const BOOKING_ERROR = `<p>Upss!</p><p>We cannot book your room.</p><p>{ERROR}.</p>`;
+
+export const UNEXPECTED_STATUS = `Received unexpected status code: {STATUS_CODE}. Please contact IT Support`;
+export const UNEXPECTED_ERROR = "Unexpected error occured";
+export const VALIDATION_ERRORS = "Validation errors have been found";
+
+export const RECEIVED_ERROR_MESSAGE = "RECEIVED_ERROR_MESSAGE";
 export const NO_ERRORS = "NO_ERRORS";
