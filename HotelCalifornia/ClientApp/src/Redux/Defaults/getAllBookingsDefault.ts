beb817a4d2e02008b8b2cbeee362dd05e3f15389
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-24 12:31:15 +0200

    fix: change default fields and theirs values

--- HotelCalifornia/ClientApp/src/Redux/Defaults/getAllBookingsDefault.ts
+++ HotelCalifornia/ClientApp/src/Redux/Defaults/getAllBookingsDefault.ts
@@ -2,11 +2,6 @@ import { IGetAllBookings } from "../States/getAllBookingsState";
 
 export const GetAllBookingsDefault: IGetAllBookings = 
 {
-    Id: "",
-    RoomNumber: 0,
-    Bedrooms: 0,
-    GuestFullName: "",
-    GuestPhoneNumber: "",
-    DateFrom: "",
-    DateTo: "",
+    isLoading: false,
+    bookings: []
 }
