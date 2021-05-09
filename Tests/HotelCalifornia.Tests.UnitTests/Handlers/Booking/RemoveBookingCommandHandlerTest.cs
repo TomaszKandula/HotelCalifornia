10d5e3e5d816a135538a7d93cb15af15918bbba3
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-05-09 13:42:43 +0200

    refactor: update assembly name

--- Tests/HotelCalifornia.Tests.UnitTests/Handlers/Booking/RemoveBookingCommandHandlerTest.cs
+++ Tests/HotelCalifornia.Tests.UnitTests/Handlers/Booking/RemoveBookingCommandHandlerTest.cs
@@ -3,7 +3,7 @@ using FluentAssertions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using HotelCalifornia.Tests.DataProviders;
+using HotelCalifornia.Backend.Core.Generators;
 using HotelCalifornia.Backend.Domain.Entities;
 using HotelCalifornia.Backend.Core.Services.DateTimeService;
 using HotelCalifornia.Backend.Cqrs.Handlers.Commands.Booking;
