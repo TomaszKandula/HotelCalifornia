5ebbd5c3363139a3281ea7c967bcfa0a4daf244a
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-05-09 13:53:40 +0200

    refactor: simplify initializer implementation

--- Backend/HotelCalifornia.Backend.Database/Initializer/DbInitializer.cs
+++ Backend/HotelCalifornia.Backend.Database/Initializer/DbInitializer.cs
@@ -1,35 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using HotelCalifornia.Backend.Database.Seeders;
 
 namespace HotelCalifornia.Backend.Database.Initializer
 {
     public class DbInitializer : IDbInitializer
     {
-        private readonly IServiceScopeFactory FScopeFactory;
+        private readonly DatabaseContext FDatabaseContext;
 
-        public DbInitializer(IServiceScopeFactory AScopeFactory)
-            => FScopeFactory = AScopeFactory;
+        public DbInitializer(DatabaseContext ADatabaseContext) => FDatabaseContext = ADatabaseContext;
 
-        public void StartMigration()
+        public void StartMigration() => FDatabaseContext.Database.Migrate();
+
+        public void SeedData()
         {
-            using var LServiceScope = FScopeFactory.CreateScope();
-            using var LDatabaseContext = LServiceScope.ServiceProvider.GetService<DatabaseContext>();
+            if (!FDatabaseContext.Rooms.Any())
+                FDatabaseContext.Rooms.AddRange(RoomsSeeder.SeedRooms());
 
-            if (LDatabaseContext?.Database.ProviderName != "Microsoft.EntityFrameworkCore.InMemory") 
-                LDatabaseContext?.Database.Migrate();
-        }
-
-        public async Task SeedData()
-        {
-            using var LServiceScope = FScopeFactory.CreateScope();
-            await using var LDatabaseContext = LServiceScope.ServiceProvider.GetService<DatabaseContext>();
-
-            if (LDatabaseContext != null && !LDatabaseContext.Rooms.AnyAsync().GetAwaiter().GetResult())
-                LDatabaseContext.Rooms.AddRange(RoomsSeeder.SeedRooms());
-
-            await LDatabaseContext?.SaveChangesAsync();
+            FDatabaseContext?.SaveChanges();
         }
     }
 }
